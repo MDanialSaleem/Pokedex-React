8bce0a18609c4ac4e6e6847d7437f7f2a9af2f38
Danial Saleem <danialsaleem2010@gmail.com>
2018-09-01 22:07:25 +0500

    Converted Inline Styles To CSS styles

--- src/App.js
+++ src/App.js
@@ -10,31 +10,11 @@ class App extends Component {
 
   render()
   {
-    const styles = {
-      backgroundImage: "linear-gradient(to bottom right, red, red 20%, blue 20%, blue 40%, white 40%, white 60%, green 60%, green 80%, yellow 80%, yellow 100%)",
-      backgroundAttachment: "fixed",
-      height: "100%",
-      width: "95%",
-      left: "50%",
-      top: "50%",
-      transform: "translate(-50%, -50%)",
-      position: "fixed",
-      //flex starts here
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-    };
+
 
     
-    if(this.props.firstEntered)
-    {
-      styles.justifyContent = "flex-start";
-      //this needs to be deleted to enable scrolling.
-      delete styles.position;
-    }
     return !this.props.loading ?
-      (<div style={styles} className="App">
+      (<div className="AppInitial">
         <Searchbar />
         {this.props.data !== null ? <BasicInfo id={this.props.id} data={this.props.data}/> : null}
         {this.props.data !== null ? <MoveCard moves={this.props.data.moves} />: null}
