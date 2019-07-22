fb8edac3f1777adb38316d2d19305b52b99dcfe7
Danial Saleem <danialsaleem2010@gmail.com>
2019-07-22 16:10:18 +0500

    types and abilities
    instantiated bootstrap
    
    replaced w bootstrp
    
    icon conversion
    
    Revert "icon conversion"
    
    This reverts commit c7b6b63b1fa8a8e39596e42be54bdc407121bb29.
    
    Revert "replaced w bootstrp"
    
    This reverts commit f693f576b2565165e1622fcd18037d75d55c24b5.
    
    Revert "instantiated bootstrap"
    
    This reverts commit b58eabd0ddd994cbc432ad06a7c8fd72784854c4.

--- src/components/BasicInfo/Ability/Ability.js
+++ src/components/BasicInfo/Ability/Ability.js
@@ -1,28 +1,6 @@
 import React from "react";
+import Chip from "@material-ui/core/Chip";
 
 export default function(props) {
-  const hiddenStyle = {
-    color: "red",
-    margin: "0px"
-  };
-
-  const abilityNameStyle = {
-    margin: "0px"
-  };
-
-  const styles = {
-    display: "inline-block",
-    boxSizing: "border-box",
-    margin: "3px",
-    border: "1px solid blue",
-    borderRadius: "5px",
-    padding: "3px"
-  };
-
-  return (
-    <div style={styles}>
-      <h3 style={abilityNameStyle}>{props.ability.ability.name}</h3>
-      {props.ability.is_hidden ? <h6 style={hiddenStyle}>Hidden</h6> : null}
-    </div>
-  );
+  return <Chip label={props.ability.ability.name} />;
 }
--- src/components/BasicInfo/Type/Type.js
+++ src/components/BasicInfo/Type/Type.js
@@ -1,29 +1,10 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 export default function(props) {
-  const importanceStyle = {
-    color: "blue",
-    margin: "0px"
-  };
-
-  const internalAbilityStyle = {
-    margin: "0px"
-  };
-
-  const style = {
-    display: "inline-block",
-    boxSizing: "border-box",
-    margin: "3px",
-    border: "1px solid blue",
-    borderRadius: "5px",
-    padding: "3px"
-  };
   return (
-    <div style={style}>
-      <h3 style={internalAbilityStyle}>{props.type.type.name}</h3>
-      <h6 style={importanceStyle}>
-        {props.type.slot === 1 ? "Primary" : "Secondary"}
-      </h6>
-    </div>
+    <Button variant={props.type.slot === 1 ? "contained" : "outlined"}>
+      {props.type.type.name}
+    </Button>
   );
 }
